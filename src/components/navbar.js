1a1a07145af8b43d3387d81ed2bdcc8ba8849e16
Marcio-Lima123 <148306104+Marcio-Lima123@users.noreply.github.com>
2025-11-20 15:36:05 -0100

    #nav_bar_progress

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,16 +1,39 @@
 import { Link } from "react-router-dom";
+import '../styles/navbar.css';
 
 export function NavBar(){
     return (
-        <header class='abc'>
-            <div>
-                <Link to='/'>Atividades</Link>
+        <header>
+            <div class='ap_container'>
+                <div class='app_logo'>
+                    {/* <img src="#" alt="App_name"></img> */}
+                </div>
+                <p>Dailio</p>
+            </div>
+            <div class='ac_container'>
+                <Link to='/daily_activity'>Atividades</Link>
                 <Link to='/classificacoes'>Classificações</Link>
             </div>
-            <div>
-                <img src="#" alt="Pfp_img"></img>
-                <p>UserName</p>
-                <Link to='/profile'></Link>
+            <div class='us_menu_container'>
+                <div class='us_menu'>
+                    <img src="#" alt="Pfp_img"></img>
+                    <p>UserName</p>
+                    <a href="#dd_menu">Menu H</a>
+                </div>
+                <div class='dd_menu' target='#dd_menu'>
+                    <div class='menu_tab'>
+                        <img alt="Img "></img>
+                        <a href='/preferencias'>Preferências</a>
+                    </div>
+                    <div class='menu_tab'>
+                        <img alt="Img "></img>
+                        <a href='/configuracoes'>Configurações</a>
+                    </div>
+                    <div class='menu_tab'>
+                        <img alt="Img "></img>
+                        <a href='/sair'>Sair</a>
+                    </div>
+                </div>
             </div>
         </header>
         
--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,25 +1,8 @@
-// import { Link } from "react-router-dom";
-
-
+import '../styles/homepage.css'
 export function Home(){
     return (
-        <div>
-            <HeaderApp />
-                <h1>Start of the project</h1>
-                {/* <Link to="/profile">Profile</Link> */}
-            <FooterApp />
+        <div class='content_container'>
+            <p>Start of the project</p>
         </div>
-    );
-}
-
-function HeaderApp(){
-    return (
-        <h1>This is the Header</h1>
-    );
-}
-
-function FooterApp(){
-    return (
-        <h1>This is a Footer</h1>
     );
 }
