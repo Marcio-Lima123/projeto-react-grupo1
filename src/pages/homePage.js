cfa5ee081b95c3ad375340c9b16ccbbb19c95f37
Marcio-Lima123 <148306104+Marcio-Lima123@users.noreply.github.com>
2025-11-22 18:15:33 -0100

    #atividades page basico quase feito

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -3,8 +3,46 @@ import '../styles/homepage.css'
 export function Home(){
     return (
         <div class='content_container'>
-            <p>Start of the project</p>
-                <a href='#/preferencias'>preferencias</a>
+            <div >
+                <h1>Atividades para Hoje (js quantidade Atividades)</h1> 
+                <div>
+                    {/* Java Script para pegar as coisas da base de dados e criar os elementos no html*/}
+                    <div class='atividade_container'>
+                        <div class='at_c'>                        
+                            <div class='at_name'>
+                                <p>Tedasdausbdiabsdadiyiausbdabsidbuabsidbaiduabsudasdxt</p>
+                            </div>
+                            <div class='etc_container'>
+                                <div class='etc_f_container'>
+                                    <div class='gc_container'>
+                                        <img src='/imgs/people_white.png' alt='people'></img>
+                                        <p>1</p>
+                                    </div>
+                                    <div class='gc_container'>
+                                        <img src='/imgs/difficulties_white.png' alt='hard_person'></img>
+                                        <p>Few to no challenges</p>
+                                    </div>
+                                </div>
+                                <div class='btn_container'>
+                                    <button type='submit'>Concluir</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div >
+            <div >
+                <h1>Atividades Recomendadas</h1>
+                <div>
+
+                </div>
+            </div >
+            <div >
+                <h1>Outras Atividades</h1>
+                <div>
+                
+                </div>
+            </div >
         </div>
     );
 }
