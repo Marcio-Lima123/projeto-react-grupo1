ab050e82b12b3cfc15949eb206aad7dcb9a8f472
Marcio-Lima123 <148306104+Marcio-Lima123@users.noreply.github.com>
2025-11-24 15:14:59 -0100

    #login page needs a little of css

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -3,20 +3,27 @@ import '../styles/login.css'
 export function LoginPage() {
     return (
         <div class='bg_container'>
-            <div class='app_name'>
+            <div class='ap_name'>
                 <img src='imgs/Logo.png' alt='Dailio_Logo'></img>
-                <h2>Dailio</h2>
+                <h1>Dailio</h1>
             </div>
             <div class='form_container'>
                 <h2>Login</h2>
-                <form>
-                    <div class='email_container'>
+                <form class='log_container'>
+                    <div class='ep_container'>
                         <label>Email</label>
                         <input value={'exemplo@gmail.com'}></input>
                     </div>
-                    <div class='pass_container'>
+                    <div class='ep_container'>
                         <label>Password</label>
                         <input value={'password'}></input>
+                    </div>
+                    <div class='btn_logIn ep_container'>
+                        <input value={'Entrar'}></input>
+                        <div class='reg_container'>
+                            <label>Não tem conta?</label>
+                            <input type='submit' value={'Registar'}></input>
+                        </div>
                     </div>
                 </form>
             </div>
