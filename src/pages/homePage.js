a1e7d508c0b08793103febb93f80e8125bea60f8
Marcio-Lima123 <148306104+Marcio-Lima123@users.noreply.github.com>
2025-11-24 10:54:42 -0100

    #caixa de atividades arranjada

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,31 +1,31 @@
-// import { Link } from 'react-router-dom';
 import '../styles/homepage.css'
-export function Home(){
+
+export function Home() {
     return (
         <div class='content_container'>
             <div >
-                <h1>Atividades para Hoje (js quantidade Atividades)</h1> 
+                <h1>Atividades para Hoje (js quantidade Atividades)</h1>
                 <div>
                     {/* Java Script para pegar as coisas da base de dados e criar os elementos no html*/}
                     <div class='atividade_container'>
-                        <div class='at_c'>                        
+                        <div class='at_c'>
                             <div class='at_name'>
                                 <p>Tedasdausbdiabsdadiyiausbdabsidbuabsidbaiduabsudasdxt</p>
                             </div>
-                            <div class='etc_container'>
-                                <div class='etc_f_container'>
-                                    <div class='gc_container'>
-                                        <img src='/imgs/people_white.png' alt='people'></img>
-                                        <p>1</p>
-                                    </div>
-                                    <div class='gc_container'>
-                                        <img src='/imgs/difficulties_white.png' alt='hard_person'></img>
-                                        <p>Few to no challenges</p>
-                                    </div>
+                        </div>
+                        <div class='at_c'>
+                            <div class='etc_f_container'>
+                                <div class='gc_container'>
+                                    <img src='/imgs/people_white.png' alt='people'></img>
+                                    <p>1</p>
                                 </div>
-                                <div class='btn_container'>
-                                    <button type='submit'>Concluir</button>
+                                <div class='gc_container'>
+                                    <img src='/imgs/difficulties_white.png' alt='hard_person'></img>
+                                    <p>Few to no challenges</p>
                                 </div>
+                            </div>
+                            <div class='btn_container'>
+                                <button type='submit'>Concluir</button>
                             </div>
                         </div>
                     </div>
@@ -40,7 +40,7 @@ export function Home(){
             <div >
                 <h1>Outras Atividades</h1>
                 <div>
-                
+
                 </div>
             </div >
         </div>
