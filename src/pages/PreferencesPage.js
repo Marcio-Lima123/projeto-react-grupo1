0dadb51aa9f450056e408fbf39e48a458ac1799a
alexandre raposo pereira <alexandrepereira06810@gmail.com>
2025-12-08 11:09:08 -0100

    #add_questionario_qts_atv

--- src/pages/PreferencesPage.js
+++ src/pages/PreferencesPage.js
@@ -5,6 +5,8 @@ export function PreferencesPage() {
     const [min, setMin] = useState(1);
     const [max, setMax] = useState(3);
 
+const [daily, setDaily] = useState(1);
+
 function inMin() {
   if (min<max) setMin(min+1);}
 function deMin() {
@@ -13,50 +15,65 @@ function inMax() {
   if (max<3) setMax(max+1);}
 function deMax() {
   if (max >min) setMax(max-1);}
+// nº atv
+    function inDaily() {
+        if (daily < 10) setDaily(daily + 1);}
+    function deDaily() {
+        if (daily > 1) setDaily(daily - 1);}
+
   return (
-    <div className="pref-page">
-      <h2 className="pref-title">Preferências</h2>
+      <div className="pref-page">
+          <h2 className="pref-title">Preferências</h2>
 
-      <div className="pref-section">
-        <h4>Tipos de Atividades:</h4>
+          <div className="pref-section">
+              <h4>Tipos de Atividades:</h4>
 
-        <div className="pref-grid">
-          <label><input type="checkbox" /> Educação</label>
-          <label><input type="checkbox" /> Recreativo</label>
-          <label><input type="checkbox" /> Social</label>
-          <label><input type="checkbox" /> Caridade</label>
-          <label><input type="checkbox" /> Cozinha</label>
-          <label><input type="checkbox" /> Relaxamento</label>
-          <label><input type="checkbox" /> Trabalho</label>
-        </div>
+              <div className="pref-grid">
+                  <label><input type="checkbox"/> Educação</label>
+                  <label><input type="checkbox"/> Recreativo</label>
+                  <label><input type="checkbox"/> Social</label>
+                  <label><input type="checkbox"/> Caridade</label>
+                  <label><input type="checkbox"/> Cozinha</label>
+                  <label><input type="checkbox"/> Relaxamento</label>
+                  <label><input type="checkbox"/> Trabalho</label>
+              </div>
+          </div>
+
+          <div className="pref-section">
+              <h4>Número de Participantes:</h4>
+              <div className="pref-number">
+                  <p>Mínimo:</p>
+                  <button onClick={deMin}>-</button>
+                  <div className="num-box">{min}</div>
+                  <button onClick={inMin}>+</button>
+
+                  <p>Máximo:</p>
+                  <button onClick={deMax}>-</button>
+                  <div className="num-box">{max}</div>
+                  <button onClick={inMax}>+</button>
+              </div>
+          </div>
+
+          <div className="pref-section">
+              <h4>Nível de Dificuldade:</h4>
+
+              <div className="pref-grid">
+                  <label><input type="checkbox"/> Fácil</label>
+                  <label><input type="checkbox"/> Médio</label>
+                  <label><input type="checkbox"/> Difícil</label>
+              </div>
+          </div>
+          <div className="pref-section">
+              <h4>Quantas atividades pretende receber por dia?</h4>
+              <div className="pref-number">
+                  <p>Quantidade:</p>
+                  <button onClick={deDaily}>-</button>
+                  <div className="num-box">{daily}</div>
+                  <button onClick={inDaily}>+</button>
+              </div>
+          </div>
+          <button className="pref-save-btn">Salvar</button>
       </div>
-
-        <div className="pref-section">
-            <h4>Número de Participantes:</h4>
-            <div className="pref-number">
-                <p>Mínimo:</p>
-                <button onClick={deMin}>-</button>
-                <div className="num-box">{min}</div>
-                <button onClick={inMin}>+</button>
-
-                <p>Máximo:</p>
-                <button onClick={deMax}>-</button>
-                <div className="num-box">{max}</div>
-                <button onClick={inMax}>+</button>
-            </div>
-        </div>
-
-        <div className="pref-section">
-            <h4>Nível de Dificuldade:</h4>
-
-            <div className="pref-grid">
-                <label><input type="checkbox"/> Fácil</label>
-                <label><input type="checkbox"/> Médio</label>
-                <label><input type="checkbox"/> Difícil</label>
-            </div>
-        </div>
-        <button className="pref-save-btn">Salvar</button>
-    </div>
   );
 }
 
